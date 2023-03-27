704827a605060b44a4c193af43c87773e5a3c68b
Gitea <gitea@fake.local>
2023-03-27 13:01:12 +0530

    Changes in the project

--- macros/adder.sql
+++ macros/adder.sql
@@ -1,7 +1,7 @@
 {% macro adder(col) %}
-    {{internal_adder(col)}} + 4
+    {{ proj2.internal_adder(col) }} + 4
 {% endmacro %}
 
 {% macro internal_adder(col) %}
-    {{col}} + 10
+    {{ col }} + 10
 {% endmacro %}
